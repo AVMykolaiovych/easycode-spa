a0f0e8372b3de2f3c76abdfb645e746c66d2ed56
avnicolaevich <avnicolaevich@gmail.com>
2018-08-21 23:50:45 +0300

    add registration ajax

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const router = new Router([
-  new Route('home', 'views/home.html'),
-  new Route('new_task', 'views/new_task.html'),
-  new Route('login', 'views/login.html', true),
-  new Route('sign_in', 'views/sign_in.html')
+    new Route('login', 'views/login.html'),
+    new Route('sign_in', 'views/sign_in.html'),
+    new Route('home', 'views/home.html'),
+    new Route('new_task', 'views/new_task.html'),
 ], 'app');
 
 router.init();
--- js/script.js
+++ js/script.js
@@ -1,7 +1,50 @@
-class SignInFormHandler {
+const BASE_URI = 'http://localhost:8000';
+
+class FormHandler {
 	constructor(form) {
 		this.form = form;
 		this.controls = this.form.elements;
+	}
+}
+
+class LoginFormHandler extends FormHandler {
+	constructor(form) {
+		super(form);
+		console.log(form)
+		Array.from(this.controls).forEach(el => el.addEventListener('blur', this.validate));
+		this.form.addEventListener('submit', this.onsubmit.bind(this));
+	}
+
+	validate(e) {
+		if(e.target.tagName === 'BUTTON') return true;
+		e.target.classList.remove('border-danger');
+
+		if(!e.target.value) {
+			e.target.classList.add('border-danger');
+			return false;
+		}
+
+		const regexp = new RegExp(e.target.dataset.reg);
+		if(!regexp.test(e.target.value)) {
+			e.target.classList.add('border-danger');
+			return false;
+		}
+
+		return true;
+	}
+
+	onsubmit(e) {
+		e.preventDefault();
+
+
+	}
+}
+
+
+class SignInFormHandler extends FormHandler{
+	constructor(form) {
+		super(form);
+		console.log(form)
 		Array.from(this.controls).forEach(el => el.addEventListener('blur', this.validate));
 		this.form.addEventListener('submit', this.onsubmit.bind(this));
 	}
@@ -33,44 +76,46 @@ class SignInFormHandler {
 
 	onsubmit(e) {
 		e.preventDefault();
+		const controls = Array.from(this.controls);
+		controls.forEach(el => this.validate({ target: el }));
+		const valid = controls.every(el => !el.classList.contains('border-danger'));
+		const email = this.form.email.value;
+		const password = this.form.password.value;
+		if(valid) {
+
+            const xhr = new XMLHttpRequest();
+            const body = JSON.stringify({ email, password });
+
+            xhr.open('POST', `${BASE_URI}/user/register`);
+			xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            xhr.withCredentials = true;
+            xhr.onloadend = function() {
+            	console.log(xhr.status);
+			};
+            xhr.onreadystatechange = function() {
+				location.replace('views/login.html')
+			};
+            xhr.onerror = function () {
+                console.log(xhr.status);
+            };
+            xhr.send(body);
+        }
 
 	}
 }
 
-class LoginFormHandler {
-	constructor(form) {
-		this.form = form;
-		this.controls = this.form.elements;
-		Array.from(this.controls).forEach(el => el.addEventListener('blur', this.validate));
-		this.form.addEventListener('submit', this.onsubmit.bind(this));
-	}
+// window.addEventListener('hashchange', () => {
+//         if (location.href === 'http://localhost:63342/easycode-spa/index.html#login') {
+//
+//         }
+//         if (location.href === 'http://localhost:63342/easycode-spa/index.html#sign_in') {
+//             const signInFormHandler = new SignInFormHandler(document.forms[0]);
+//         }
+// });
 
-	validate(e) {
-		if(e.target.tagName === 'BUTTON') return true;
-		e.target.classList.remove('border-danger');
+document.querySelector('a[href="#login"]').addEventListener('click', () => new LoginFormHandler(document.forms[0]));
+document.querySelector('a[href="#sign_in"]').addEventListener('click', () => new SignInFormHandler(document.forms[0]));
 
-		if(!e.target.value) {
-			e.target.classList.add('border-danger');
-			return false;
-		}
 
-		const regexp = new RegExp(e.target.dataset.reg);
-		if(!regexp.test(e.target.value)) {
-			e.target.classList.add('border-danger');
-			return false;
-		}
 
-		return true;
-	}
-
-	onsubmit(e) {
-		e.preventDefault();
-
-	}
-}
-
-window.onload = function() {
-    const signInFormHandler = new SignInFormHandler(document.forms[0]);
-    const loginFormHandler = new LoginFormHandler(document.forms[0]);
-};
 
